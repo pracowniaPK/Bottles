b0ca6b406cab77a87671fdd352600cec22968f80
PKacz <kaczory5@gmail.com>
2018-12-24 12:47:29 +0100

    minor fixes

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,7 +4,3 @@ from bottles import create_app
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
-
-def test_hello(client):
-    response = client.get('/hello')
-    assert response.data == b'dupa'
