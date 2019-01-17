4c98f2304f471f14359c76a0d0663211e001b181
PKacz <kaczory5@gmail.com>
2019-01-17 23:30:26 +0100

    change configuration process

--- bottles/__init__.py
+++ bottles/__init__.py
@@ -4,21 +4,19 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
+    app.config.from_object(os.environ['APP_SETTINGS'])
+
     app.config.from_mapping(
-        SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
     )
-
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    if test_config is not None:
+        app.config.from_mapping(test_config)
 
     from . import db
     db.init_app(app)
